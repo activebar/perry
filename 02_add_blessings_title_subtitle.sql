846d48596001965df67a3db10403c032c6025d06
activebar <activebararchive@gmail.com>
2026-02-02 09:03:53 +0200

    Delete 02_add_blessings_title_subtitle.sql

--- 02_add_blessings_title_subtitle.sql
+++
@@ -1,4 +0,0 @@
--- Add configurable Blessings block title + subtitle (used on Home + Blessings pages)
-alter table public.event_settings
-  add column if not exists blessings_title text,
-  add column if not exists blessings_subtitle text;
