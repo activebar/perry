4e6889ede61e74296d62d14cb82d52048ba02990
activebar <activebararchive@gmail.com>
2026-03-05 16:06:50 +0200

    Update route.ts

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 import sharp from "sharp"
-import { supabaseServiceRole } from "@/lib/supabase/server"
-
+import { supabaseServiceRole } from "../../../../lib/supabase/server"
 export const dynamic = "force-dynamic"
 
 export async function POST(req: NextRequest) {
